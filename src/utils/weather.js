f35b061a0083c0e2738997e640b91dd0146ad779
dalmaszekely <dalmaszekely@users.noreply.github.com>
2020-08-20 12:18:04 +0200

    More details for the forecast

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -8,7 +8,7 @@ const weather = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, 'Currently it is ' + body.current.temperature + ' celsius degree in ' + body.location.name + ' and it is a ' + body.current.precip + '% chance of raining today.')
+            callback(undefined, 'Currently it is ' + body.current.temperature + ' celsius degree in ' + body.location.name + ' and it is a ' + body.current.precip + '% chance of raining today. The speed of the wind is ' + body.current.wind_speed + 'km/h, which makes the temperature feel like ' + body.current.feelslike + ' celsius degree.')
         }
     })
 }
